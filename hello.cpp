42130e6c45a1198b165009726cabb406066cf59d
ChRis98Wang <87601692+ChRis98Wang@users.noreply.github.com>
2024-04-23 23:16:28 +0200

    Begin hello algo

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
+//begin Hello Algo
+int whileloop(int n ){
+        int res = 0;
+        int i = 1;
+        while (i<=n)
+        {
+            res +=i;
+            i++;
+        }
+        return res;
+        
+    }
 int main(){
-
+    std::cout<<whileloop(4)<<std::endl;
+    
     std::cout<<"hello github"<<std::endl;
     return 0;
 }
